50b408590b6596212a1f23e05daf061bd92fb835
dhilip109 <dhilipmbox@gmail.com>
2022-03-25 19:37:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const loadMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width : 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.on("ready", loadMainWindow);
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        loadMainWindow();
+    }
+});
